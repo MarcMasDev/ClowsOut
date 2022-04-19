cd80c13f47a740016959772a5a4ec7ac3b42a9c5
edu1010 <39130123+edu1010@users.noreply.github.com>
2022-04-19 16:54:51 +0200

    patrolAI

--- Assets/Scripts/FSM/Enemies/BlackboardEnemies.cs
+++ Assets/Scripts/FSM/Enemies/BlackboardEnemies.cs
@@ -11,9 +11,15 @@ public class BlackboardEnemies : MonoBehaviour
     public float m_MoveDistanceAfterAttack = 8f;
     public bool m_FinishAttack = false;
     public float m_distanceToPlayer;
+    public Transform m_ParentWaypoints;
+    public Transform[] m_Waypoints;
+    public float m_DetectionDistance = 100f;
+    public float m_AngleVision = 60f;
+    public LayerMask m_CollisionLayerMask;
     private void Awake()
     {
         m_Player = GameObject.FindGameObjectWithTag("Player").transform;
         m_distanceToPlayer = Vector3.Distance(m_Player.position, transform.position);
+        m_Waypoints = m_ParentWaypoints.GetComponentsInChildren<Transform>();
     }
 }
