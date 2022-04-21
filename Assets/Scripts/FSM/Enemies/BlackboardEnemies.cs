60104f5a357fa5200f8d60dc2a447e9428d8157f
edu1010 <39130123+edu1010@users.noreply.github.com>
2022-04-22 00:48:21 +0200

    improving patrol

--- Assets/Scripts/FSM/Enemies/BlackboardEnemies.cs
+++ Assets/Scripts/FSM/Enemies/BlackboardEnemies.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BlackboardEnemies : MonoBehaviour
 {
+    public float m_Speed = 5f;
     public Transform m_Player;
     public float m_RangeAttack = 15f;
     public float m_IdealRangeAttack = 10f;
@@ -12,11 +13,13 @@ public class BlackboardEnemies : MonoBehaviour
     public bool m_FinishAttack = false;
     public float m_distanceToPlayer;
     public Transform m_ParentWaypoints;
-    public Transform[] m_Waypoints;
     public float m_DetectionDistance = 100f;
     public float m_AngleVision = 60f;
-    public LayerMask m_CollisionLayerMask;
+    public LayerMask m_CollisionLayerMask;  
+    public Transform[] m_Waypoints;
+   
     public HighFSM.States m_PreviusState;
+    
     private void Awake()
     {
         m_Player = GameObject.FindGameObjectWithTag("Player").transform;
