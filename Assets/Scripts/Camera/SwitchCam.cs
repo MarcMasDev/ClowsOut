b6ede87ee85bea2e3e30f30759e97e402c80bb6c
ryk0-lab <ricardpelaezc@gmail.com>
2022-04-25 01:04:04 +0200

    Dash

--- Assets/Scripts/Camera/SwitchCam.cs
+++ Assets/Scripts/Camera/SwitchCam.cs
@@ -5,29 +5,29 @@ using UnityEngine;
 
 public class SwitchCam : MonoBehaviour
 {
-    [SerializeField]
-    InputManager m_InputManager;
-    [SerializeField]
-    CinemachineVirtualCamera m_aimCamera;
-    [SerializeField]
-    int m_IncreseCamPriority = 10;
     private void OnEnable()
     {
-        m_InputManager.OnStartAiming += SwitchToAimCamera;
-        m_InputManager.OnStopAiming += SwitchToThirdCamera;
+        InputManager.Instance.OnStartAiming += SwitchToAimCamera;
+        InputManager.Instance.OnStopAiming += SwitchToThirdCamera;
     }
     private void OnDisable()
     {
-        m_InputManager.OnStartAiming -= SwitchToAimCamera;
-        m_InputManager.OnStopAiming -= SwitchToThirdCamera;
+        InputManager.Instance.OnStartAiming -= SwitchToAimCamera;
+        InputManager.Instance.OnStopAiming -= SwitchToThirdCamera;
     }
 
     public void SwitchToAimCamera()
     {
-        m_aimCamera.Priority += m_IncreseCamPriority;
+        if (!CameraManager.Instance.m_Locked)
+        {
+            CameraManager.Instance.m_aimCamera.Priority += CameraManager.Instance.m_IncreseCamPriority;
+        }
     }
     public void SwitchToThirdCamera()
     {
-        m_aimCamera.Priority -= m_IncreseCamPriority;
+        if (!CameraManager.Instance.m_Locked)
+        {
+            CameraManager.Instance.m_aimCamera.Priority -= CameraManager.Instance.m_IncreseCamPriority;
+        }
     }
 }
