5704beb193dca23563993684996828b251ecf533
edu1010 <39130123+edu1010@users.noreply.github.com>
2022-05-11 15:54:31 +0200

    enemy sticky

--- Assets/Scripts/FSM/Enemies/AtackFSMSticky.cs
+++ Assets/Scripts/FSM/Enemies/AtackFSMSticky.cs
@@ -90,7 +90,7 @@ public class AtackFSMSticky : FSM_AI
     }
     public void Shoot()
     {
-        Vector3 l_Dispersion = new Vector3(Random.Range(0, 0.5f), Random.Range(0, 0.5f), Random.Range(0, 0.5f));
+        Vector3 l_Dispersion = new Vector3(Random.Range(-1f, 1f), Random.Range(-0.5f, -1f), Random.Range(-1f, 1f));
         Vector3 l_bulletDir = ((m_blackboardEnemies.m_Player.position + l_Dispersion) - m_firepoint.position).normalized;
         m_shootSystem.BulletShoot(m_firepoint.position, l_bulletDir, m_BulletSpeed, m_bulletType);
     }
