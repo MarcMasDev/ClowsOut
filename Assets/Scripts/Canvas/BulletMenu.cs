7fac5d868810a94b177a251b379d404097d4ed16
AinoaTA <atabares@edu.tecnocampus.cat>
2022-05-20 18:28:17 +0200

    shootsytem manager

--- Assets/Scripts/Canvas/BulletMenu.cs
+++ Assets/Scripts/Canvas/BulletMenu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
-using static ShootSystem;
+using static ShootSystemManager;
 
 public class BulletMenu : MonoBehaviour
 {
