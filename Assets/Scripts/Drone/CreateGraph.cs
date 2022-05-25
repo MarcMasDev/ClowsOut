c9177ed6bc0662a2206fd67344fa7b76a4667ffc
edu1010 <39130123+edu1010@users.noreply.github.com>
2022-05-25 04:08:43 +0200

    generate graph first step

---
+++ Assets/Scripts/Drone/CreateGraph.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateGraph : MonoBehaviour
+{
+    [SerializeField]
+    GameObject m_node;
+    [SerializeField]
+    Transform m_StartPosition;
+    [SerializeField]
+    float m_SeperationBetweenNodes = 0.5f;
+    [SerializeField]
+    float m_length = 100;
+    int counter = 0;
+    private void Start()
+    {
+        SpawnNodes();
+    }
+    public void SpawnNodes()
+    {
+        float l_quantity = (m_length / m_SeperationBetweenNodes)* (m_length / m_SeperationBetweenNodes)* (m_length / m_SeperationBetweenNodes);
+        float l_quantityForRow = m_length / m_SeperationBetweenNodes;
+        for (int z = 0; z < l_quantityForRow; z++)//Pos z
+        {
+            for (int y = 0; y < l_quantityForRow; y++)
+            {
+                for (int x = 0; x < l_quantityForRow; x++)
+                {
+                    Vector3 l_pos = m_StartPosition.position + new Vector3(x*m_SeperationBetweenNodes,y* m_SeperationBetweenNodes,z* m_SeperationBetweenNodes);
+                   GameObject go = Instantiate(m_node, l_pos, Quaternion.identity, m_StartPosition);
+                    go.name = counter.ToString();
+                    counter++;
+                }
+            }
+        }
+    }
+}
