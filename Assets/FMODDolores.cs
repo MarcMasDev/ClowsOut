f8a76eaf9cd87ae9e3c43380877c9493855a3291
MarcMasDev <89383983+MarcMasDev@users.noreply.github.com>
2022-06-02 20:24:48 +0200

    FMOD

--- Assets/FMODDolores.cs
+++ Assets/FMODDolores.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using FMODUnity;
 public class FMODDolores : MonoBehaviour
 {
+    [SerializeField] private StudioEventEmitter footstep;
     public void FootStep()
     {
         //Sound
+        footstep.Play();
     }
     public void Shoot()
     {
         //Sound
+
     }
 }
