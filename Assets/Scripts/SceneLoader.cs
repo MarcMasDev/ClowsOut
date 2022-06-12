1b2898c66898546485a394fc992af46a56bb5941
AinoaTA <atabares@edu.tecnocampus.cat>
2022-06-12 22:44:54 +0200

    scena

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,7 +5,10 @@ using UnityEngine.SceneManagement;
 public class SceneLoader : MonoBehaviour
 {
     private SceneLoader _instance;
-   
+
+    [Tooltip("dont touch this array. Look LevelData name levels")]
+    [SerializeField]private string[] LevelNames;
+    [SerializeField] private string LoadingSceneName;
     private void Awake()
     {
         if (_instance != null && _instance != this)
@@ -22,18 +25,18 @@ public class SceneLoader : MonoBehaviour
     private void Start()
     {
         GameManager.GetManager().SetSceneLoader(this);
+        LevelNames = GameManager.GetManager().GetLevelData().m_SceneNames;
     }
-    public string[] LevelNames;
-
-    public string LoadingSceneName;
+   
 
     /// <summary>
     /// Load desired scene with out loading scene.
     /// </summary>
     /// <param name="level"></param>
+    /// 
     public void LoadLevel(int level)
     {
-        if (LoadingSceneName == LevelNames[level])
+        if (LoadingSceneName != LevelNames[level] && LevelNames.Length>level)
         {
             LoadingSceneName = LevelNames[level];
             Debug.Log(LevelNames[level] + "scene loaded with exit");
@@ -55,14 +58,14 @@ public class SceneLoader : MonoBehaviour
     /// <param name="scene"></param>
     public void LoadWithLoadingScene(int level)
     {
-        if (LoadingSceneName == LevelNames[level])
+        if (LoadingSceneName != LevelNames[level] && LevelNames.Length > level)
         {
             LoadingSceneName = LevelNames[level];
             Debug.Log(LevelNames[level] + "scene loaded with exit");
             StartCoroutine(LoadLoadingScene(level));
         }
         else
-            Debug.Log(level + "level doesn't exit. Cant be loaded.");
+            Debug.Log(level + " level doesn't exit or is already loaded.");
     }
     IEnumerator LoadLoadingScene(int scene)//TODO
     {
@@ -70,9 +73,28 @@ public class SceneLoader : MonoBehaviour
         //also load loading scene
         SceneManager.LoadSceneAsync("Loading");
         yield return new WaitForSeconds(0.5f);
-       // SceneManager.UnloadScene(SceneManager.GetActiveScene().buildIndex-1);
+        TextEffects l_effects = FindObjectOfType<TextEffects>();
         AsyncOperation LoadLevel = SceneManager.LoadSceneAsync(scene);
+
+        LoadLevel.allowSceneActivation = false;
+        Debug.Log("Pro :" + LoadLevel.progress);
         //LoadLevel.progress =1 loafing finish => update progresion bar 
+        while (!LoadLevel.isDone)
+        { 
+            Debug.Log("Pro :" + LoadLevel.progress);
+            l_effects.m_TextPercentatge.text = "Loading progress: " + (LoadLevel.progress * 100) + "%";
+            // Check if the load has finished
+            if (LoadLevel.progress >= 0.9f)
+            {
+                //Change the Text to show the Scene is ready
+               // l_effects.m_TextPercentatge.text = "Press the space bar to continue";
+                //Wait to you press the space key to activate the Scene
+                //////////////if (Input.GetKeyDown(KeyCode.Space))
+                //////////////    //Activate the Scene
+                LoadLevel.allowSceneActivation = true;
+            }
+            yield return null;
+        }
         LoadLevel.completed += (asyncOperation) =>
         {
             //If we need to set something when finish
