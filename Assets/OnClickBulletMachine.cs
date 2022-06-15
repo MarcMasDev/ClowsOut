35d1f422cd455d7bb1caa2a0bd0e7ee4db24d121
edu1010 <39130123+edu1010@users.noreply.github.com>
2022-06-15 08:17:29 +0200

    fix bug bulletmachine

--- Assets/OnClickBulletMachine.cs
+++ Assets/OnClickBulletMachine.cs
@@ -1,17 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class OnClickBulletMachine : MonoBehaviour
+public class OnClickBulletMachine : MonoBehaviour,IPointerClickHandler
 {
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        GameManager.GetManager().GetCameraManager().m_SwitchCam.SwitchToNotAimingCamera(); 
+        GameManager.GetManager().GetCanvasManager().ShowIngameMenu();
+    }
     public void OnAcceptBulletSwitchCam()
     {
-        GameManager.GetManager().GetCameraManager().m_SwitchCam.SwitchToNotAimingCamera();
+       // GameManager.GetManager().GetCameraManager().m_SwitchCam.SwitchToNotAimingCamera();
         //GameManager.GetManager().GetCanvasManager().ShowReticle();
     } 
     public void OnAcceptRestTimeScale()
     {
-        GameManager.GetManager().GetCanvasManager().ShowIngameMenu();
+       // GameManager.GetManager().GetCanvasManager().ShowIngameMenu();
     }
 
 }
