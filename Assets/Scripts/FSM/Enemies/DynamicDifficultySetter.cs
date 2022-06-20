b35432fe50f27ac1636a6bbe8a64c6bdf65d2a14
MarcMasDev <89383983+MarcMasDev@users.noreply.github.com>
2022-06-20 23:46:52 +0200

    difficultySetterOptions

--- Assets/Scripts/FSM/Enemies/DynamicDifficultySetter.cs
+++ Assets/Scripts/FSM/Enemies/DynamicDifficultySetter.cs
@@ -6,6 +6,7 @@ public static class DynamicDifficultySetter
 {
     private static float damageFormula = -1;
     private static float shootFormula = -1;
+    private static bool apply = true;
     public static void SetDifficulty()
     {
         damageFormula = GameManager.GetManager().GetLevelData().LoadDeathsPlayer() - GameManager.GetManager().GetCurrentRoomIndex()*2;
@@ -13,13 +14,22 @@ public static class DynamicDifficultySetter
     }
     public static float GetDamage(float baseDamage)
     {
+        if (!apply)
+            return baseDamage;
+
         if (damageFormula < 1) { damageFormula = 1; }
         return baseDamage / damageFormula*0.75f;
     }
     public static float GetShootSpeed(float baseShootSpeed)
     {
+        if (!apply)
+            return baseShootSpeed;
+
         if (shootFormula == -1) SetDifficulty();
         return baseShootSpeed + shootFormula;
     }
-
+    public static void SetDifficultyBool(bool dynDiff)
+    {
+        apply = dynDiff;
+    }
 }
