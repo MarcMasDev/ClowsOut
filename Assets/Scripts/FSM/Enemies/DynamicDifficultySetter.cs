577cb473415a56a931f9ecb3f50ac4d67d85089f
MarcMasDev <89383983+MarcMasDev@users.noreply.github.com>
2022-06-21 20:22:35 +0200

    balance changes

--- Assets/Scripts/FSM/Enemies/DynamicDifficultySetter.cs
+++ Assets/Scripts/FSM/Enemies/DynamicDifficultySetter.cs
@@ -7,20 +7,29 @@ public static class DynamicDifficultySetter
     private static float damageFormula = -1;
     private static float shootFormula = -1;
     private static bool apply = true;
+    private static bool serialized = false;
     public static void SetDifficulty()
     {
-        damageFormula = GameManager.GetManager().GetLevelData().LoadDeathsPlayer() * 0.75f - GameManager.GetManager().GetCurrentRoomIndex() * 0.75f;
+        damageFormula = GameManager.GetManager().GetLevelData().LoadDeathsPlayer() * 0.3f - GameManager.GetManager().GetCurrentRoomIndex() * 0.3f;
         shootFormula = GameManager.GetManager().GetLevelData().LoadDeathsPlayer() * 0.25f - GameManager.GetManager().GetCurrentRoomIndex() * 0.25f;
     }
     public static float GetDamage(float baseDamage)
     {
-        if (!apply || baseDamage == 20)
+        if (!apply)
             return baseDamage;
         
+        if (!serialized)
+        {
+            serialized = true;
+            SetDifficulty();
+        }
+
         float shootsToKill = 100 / baseDamage  + damageFormula;
 
-        if (shootsToKill < 3) shootsToKill = 3;
+        if (shootsToKill < 4) shootsToKill = 4;
+        else if (shootsToKill > 6.1f) shootsToKill = 6.5f;
 
+        Debug.Log("DEB_SHOOTS TO KILL = " + shootsToKill + " // Real Damage = " + 100 / shootsToKill);
         return 100/shootsToKill;
     }
     public static float GetShootSpeed(float baseShootSpeed)
@@ -28,6 +37,14 @@ public static class DynamicDifficultySetter
         if (!apply)
             return baseShootSpeed;
 
+        if (!serialized)
+        {
+            serialized = true;
+            SetDifficulty();
+        }
+        Debug.Log("DEB_ShootSpeed = " + shootFormula + " // current: " + (baseShootSpeed + shootFormula));
+        if (shootFormula > 1) shootFormula = 1;
+        if (shootFormula < 0) shootFormula = 0;
         return baseShootSpeed + shootFormula;
     }
     public static void SetDifficultyBool(bool dynDiff)
